376a2b5cbbf72edb5bec4ec426db3ef27cc63207
Pratama <pratama27649@example.com>
2025-12-27 11:49:57 +0700

    learn for and while loop in c

---
+++ loop.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int i = 1;
+    while (i <= 3){
+        printf("meow while loop\n");
+        i++;
+    }
+    for (int i =0;i<4;i++){
+        printf("meow for loop\n");
+    }
+}
