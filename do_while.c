5b6205b7e7ed9d5e8d54726017a06ff60cf58e11
Pratama <pratama27649@example.com>
2025-12-28 09:42:32 +0700

    learn do while in c

--- do_while.c
+++ do_while.c
@@ -1,16 +1,23 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(){
     int i = 0;
-    char names[4][20];
+    char a_convert_to_int[8];
+    int a;
+
+    printf("how much you want to enter a name?\n");
+    fgets(a_convert_to_int,sizeof a_convert_to_int,stdin);
+    a = atoi(a_convert_to_int);
+    char names[a][20];
     do{
         printf("enter name %d: ",i+1);
         fgets(names[i],sizeof names[i],stdin);
 
         i++;
-    }while(i<4);
+    }while(i<a);
     printf("\nyou entered: \n");
-    for (int j = 0; j<4;j++){
+    for (int j = 0; j<a;j++){
         printf("%d:%s",j+1,names[j]);
     }
     return 0;
